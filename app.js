f43b29f43516044450b185502e45dc31cac1e585
unknown <CodeGabut>
2025-07-05 13:15:19 +0700

    Percobaan Saja

---
+++ app.js
@@ -0,0 +1,36 @@
+
+const fs = require("fs") ;
+const express = require('express') ; 
+const path = require('path');
+const app = express() ; 
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+let data = {
+    misal  : "Dodi"
+}
+
+app.get('/create_article', (req, res) => {
+fs.writeFile('data.json',JSON.stringify(data), (data,error) =>{
+console.log("Berhasilah pokok e") ; 
+}); 
+res.render('form.ejs') ; 
+  
+})
+app.post('/create_article', (req, res) => {
+ console.log(req.body);
+ res.send("jadi") ; 
+ 
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+ 
+
+
+
+
