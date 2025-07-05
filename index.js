fad407e6d7c67334fe175d487af92ca087270742
unknown <CodeGabut>
2025-07-05 15:07:04 +0700

    Menghapus port

--- index.js
+++ index.js
@@ -9,20 +9,15 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-let data = {
-    misal  : "Dodi"
-}
+
 
 app.get('/', (req, res) => {
-
-res.render('form.ejs') ; 
+res.send('woilah') ; 
   
 })
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
  
 
 
